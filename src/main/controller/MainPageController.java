7a1a75c8f7c3560182f2fec9c5c3fc9d9cb1c397
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2021-01-27 16:40:57 +0200

    ref

--- src/main/controller/MainPageController.java
+++ src/main/controller/MainPageController.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Clock;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-import com.sun.scenario.effect.impl.sw.java.JSWBlend_GREENPeer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -18,7 +16,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import main.model.*;
 import javafx.scene.text.Text;
@@ -75,15 +72,7 @@ public class MainPageController {
     @FXML
     void initialize() {
         stopThreads = true;
-//        if (!Agent.isGetUpToDateDataOnPairs()) {
-//            System.out.println("000");
-//            Thread thread = new Thread(new GetUpToDateDataOnPairs());
-//            thread.start();
-//            try { thread.join(); }
-//            catch (InterruptedException e) { e.printStackTrace(); }
-//        }
-        Thread clock = new Thread(new Clock());
-        clock.start();
+        new Thread(new Clock()).start();
         Thread showExchangeRatesAndStatusBar = new Thread(new ShowExchangeRatesAndStatusBar());
         showExchangeRatesAndStatusBar.start();
 
@@ -132,10 +121,6 @@ public class MainPageController {
 //            arraysOfWebSockets.stopAllWebSocketsForWorkingCouples();
         });
 
-        timeText.setOnMouseClicked(mouseEvent -> {
-            if (mouseEvent.getClickCount() == 2) { openNewScene("/main/view/time.fxml"); }
-        });
-
         startButton.setOnAction(event -> {
             new StartButton().start();
 //            String string = getSelectedItem();
@@ -170,10 +155,9 @@ public class MainPageController {
             if (string.length() > 5) arraysOfStrategies.removeStrategy(string);
         });
 
-
-//        if (!Agent.isGetUpToDateDataOnPairs()) {
-//            openNewScene("/main/view/authorization.fxml");
-//        }
+        timeText.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getClickCount() == 2) { openNewScene("/main/view/time.fxml"); }
+        });
     }
 
 
@@ -219,35 +203,6 @@ public class MainPageController {
         Stage stage = new Stage();
         stage.setScene(new Scene(parent));
         stage.show();
-
-
-//        Alert alert = new Alert(AlertType.CONFIRMATION);
-//        alert.setTitle("Select");
-//        alert.setHeaderText("Choose the sport you like:");
-//
-//        ButtonType football = new ButtonType("Football");
-//        ButtonType badminton = new ButtonType("Badminton");
-//        ButtonType volleyball = new ButtonType("Volleyball");
-//
-//        // Remove default ButtonTypes
-//        alert.getButtonTypes().clear();
-//
-//        alert.getButtonTypes().addAll(football, badminton, volleyball);
-//
-//        // option != null.
-//        Optional<ButtonType> option = alert.showAndWait();
-
-//        if (option.get() == null) {
-//            this.label.setText("No selection!");
-//        } else if (option.get() == football) {
-//            this.label.setText("You like Football");
-//        } else if (option.get() == badminton) {
-//            this.label.setText("You like Badminton");
-//        } else if (option.get() == volleyball) {
-//            this.label.setText("You like Volleyball");
-//        } else {
-//            this.label.setText("-");
-//        }
     }
 
 
